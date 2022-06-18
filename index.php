9be5fe479e65462c149d7a282ae14a6a9493859d
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-06-18 12:15:05 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,14 +10,59 @@
     <title>Eyes | Login</title>
 </head>
 <body>
-    
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm">
+    <nav class="nav-tabs">
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+            <a class="nav-link active" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Cadastro</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="https://github.com/Felipesco/eyes">Saiba Mais</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#">CVE Store</a>
+        </li>
+        </ul>
+    </nav>
+
+    <div class="container-fluid" style="margin-top: 2%;">
+        <div class="row justify-content-around">
+            <div class="col-4">
                 Login
             </div>
-            <div class="col-sm">
-                Cadastro
+            <div class="col-4">
+                <form method="POST" action="cadastro.php" onsubmit="return valida_form_cadastro(this)">
+                    <div class="form-group row" >
+                        <label for="formGroupExampleInput">
+                            Nome
+                        </label>
+                        <input class="form-control" placeholder="Nome" type="text" name="nomeUser" id="nomeUser">
+                    </div>
+
+                    <div class="form-group row" >
+                        <label for="formGroupExampleInput">
+                            E-mail
+                        </label>
+                        <input class="form-control" placeholder="exemplo@exemplo.com" type="email" name="emailUser" id="emailUser">
+                    </div>
+
+                    <div class="form-group row" >
+                        <label for="formGroupExampleInput">
+                            Senha
+                        </label>
+                        <input class="form-control" type="password" name="senhaUser" id="senhaUser">
+                    </div>
+
+                    <div class="form-group row" >
+                        <label for="formGroupExampleInput">
+                            Telefone
+                        </label>
+                        <input class="form-control" placeholder="123456789" type="number" name="telefoneUser" id="telefoneUser">
+                    </div>
+                        <input type="submit" value="Cadastrar" id="cadastrar" name="cadastrar">
+                </form>
             </div>
         </div>
     </div>
@@ -26,7 +71,19 @@
 
 
 
-
+    <script language='javascript' type='text/javascript'> 
+        function valida_form_cadastro(){
+            if (document.getElementById("emailUser").value == ""){
+                alert('Preencha o campo de E-mail')
+            }
+            else if (document.getElementById("senhaUser").value == ""){
+                alert('Preencha o campo de Senha')
+            }
+            else if (document.getElementById("senhaUser").value.length < 3 ){
+                alert('Senha Muito Fraca \nEscolha uma senha com no minímo 4 caracteres')
+            }
+        }
+    </script>
 
     <!--Bootstrap JS-->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
