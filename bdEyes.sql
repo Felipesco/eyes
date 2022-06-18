8cc29778257428447e18e9719261c750d9685abc
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-06-18 10:14:02 -0300

    Update bdEyes.sql

--- bdEyes.sql
+++ bdEyes.sql
@@ -1,7 +1,21 @@
 CREATE DATABASE bdEyes
 USE bdEyes
 
-CREATE TABLE tbVulnerabilidades(
+CREATE TABLE tbVuln(
+    idVuln INT PRIMARY KEY AUTO_INCREMENT
+    , nomeVuln VARCHAR(80) 
+    , tipoVuln VARCHAR(50) 
+    , descricaoVuln VARCHAR(200)
+	, cveidVuln VARCHAR(35)
+    , refenciaVuln VARCHAR(200)
+    , riscoVuln INT 
+    , imgVuln LONGBLOB
+);
 
-
-)
+CREATE TABLE tbAlvo(
+    idAlvo INT PRIMARY KEY AUTO_INCREMENT
+    , FOREIGN KEY(idVuln) REFERENCES tbVuln(idVuln)
+    , nomeAlvo VARCHAR(100) NOT NULL
+    , descricaoAlvo VARCHAR(200)
+    , urlAlvo VARCHAR(200) NOT NULL 
+);
