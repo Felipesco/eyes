859e77aea389d1873e1d5b9b1017a3d54d34489e
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-06-18 12:39:02 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -6,6 +6,17 @@ $emailUser = $_POST['emailUser'];
 $senhaUser = MD5($_POST['senhaUser']);
 $telefoneUser = $_POST['telefoneUser']; 
 
+# Mensagem de Erro
+# Error: SQLSTATE[21S01]: Insert value list does not match column list: 1136 Column count doesn't match value count at row 1
 
-header("Location:index.php");
+try{
+    $stmt = $pdo->prepare("insert into tbUser values(
+        null,'$nomeUser','$emailUser','$senhaUser','$telefoneUser', null, null
+    )");
+    $stmt->execute();
+}
+catch(PDOException $e) {
+    echo 'Error: ' . $e->getMessage();
+}	
+
 ?>
