98e3b52f1dbc9f4c115f54e6a6638885414db016
Felipe Santos <72233926+Felipesco@users.noreply.github.com>
2022-06-18 21:35:42 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -5,4 +5,6 @@
 	$senha="";
 
 	$pdo = new PDO("mysql:host=$servidor;dbname=$banco",$usuario,$senha);		
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec("SET CHARACTER SET utf8");
 ?>
